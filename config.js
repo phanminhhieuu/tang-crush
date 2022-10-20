77761819d7b94ef40c4406af519426a86dfb508a
phanminhhieuu <85856691+phanminhhieuu@users.noreply.github.com>
2022-10-20 12:23:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: 'Hello bạn!',
-    introDesc: `Tui là Híu nè ,
-    btnIntro: 'hihi',béo lên gòi chứ gì :v
-    title: 'Có phải dạo này bạn Thủy của tui đang béo lên gòi hong :))',
+    introTitle: 'Hello',
+    introDesc: `Con là Hiếu nè ,
+    btnIntro: 'hihi'
+    title: ' :))',
     desc: ' ',
-    btnYes: 'Béo kệ tui :((',
-    btnNo: 'Tui hong có béo nhaaa :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnYes: '1',
+    btnNo: '2',
+    question:'Cảm ơn nhiềuuuu <3',
     btnReply: 'Gửi cho Hiếuuuuu',
-    reply: 'Béo thì béo mà hong béo thì thuiii <33333333',
-    mess: 'Tui biết mà :)))🥰',
-    messDesc: 'Thoi ko sao béo chút vẫn dễ thương hehehe.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: '<33333333',
+    mess: '🥰',
+    messDesc: 'happy women day.',
+    btnAccept: '<3',
     messLink: 'https://www.facebook.com/profile.php?id=100065287531042
 }
